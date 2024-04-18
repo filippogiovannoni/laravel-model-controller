de49bb33e6b0b5695d035ffaefc4109037b3289c
filippogiovannoni <filippogiovannonii@gmail.com>
2024-04-18 11:28:51 +0200

    add header partial and yield for main content

--- resources/views/guests/homepage.blade.php
+++ resources/views/guests/homepage.blade.php
@@ -1,1 +1,21 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-3 p-3">
+                    <div class="card">
+                        <img style="object-fit: cover; aspect-ratio: 3/4" src="{{ $movie['img_url'] }}" class="card-img-top"
+                            alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie['title'] }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted ">{{ $movie['original_title'] }}</h6>
+                            <p class="card-text">{{ $movie['vote'] }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,58 +18,11 @@
 
 <body class="antialiased">
 
-    <header>
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="#">Movies DB</a>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#" aria-current="page">Home
-                                <span class="visually-hidden">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex my-2 my-lg-0">
-                        <input class="form-control me-sm-2" type="text" placeholder="Search" />
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-                            Search
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-    </header>
+    @include('partials.header')
 
-
-    <div class="container">
-        <div class="row">
-            @foreach ($movies as $movie)
-                <div class="col-3">
-                    <div class="card gap-3" style="width:18rem;">
-                        <img src="{{ $movie['img_url'] }}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $movie['title'] }}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted ">{{ $movie['original_title'] }}</h6>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                                of
-                                the
-                                card's
-                                content.</p>
-                            b5
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
+    <main class="my-3">
+        @yield('content')
+    </main>
 
 
 
